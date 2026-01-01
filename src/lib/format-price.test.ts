a707e2acdcdace92796bf6ca9ec68b5f0614f6f2
Calliste Ravix <calliste.ravix@gmail.com>
2026-01-01 14:42:59 +0100

    test: Remove obsolete `formatPrice` utility tests

--- src/lib/format-price.test.ts
+++
@@ -1,8 +0,0 @@
-import { expect, test } from "vitest";
-import { formatPrice } from "./format-price";
-
-test("Formate correctement le prix en euros", () => {
-  expect(formatPrice(10)).toBe("10,00 €");
-  expect(formatPrice(15.5)).toBe("15,50 €");
-  expect(formatPrice(0)).toBe("0,00 €");
-});
